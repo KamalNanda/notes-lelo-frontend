0d20d2f285655cfc4d35e543c1301ab82a0b6616
Kamal Nanda <kamalnanda45@gmail.com>
2020-08-07 21:13:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import axios from 'axios'
 import NotesList from './Components/noteslist.js'
 import AddNotes from './Components/addNote'
 import {Switch , Route } from 'react-router'
+import SocialLogin from './Components/SocialLogin'
+import Register from './Components/Register'
+import NotesPage from './Components/NotesPage'
+import SignUpPage from './Components/SignUpPage'
+const uri = "http://localhost:2000"
 class App extends React.Component{
     constructor(props){
         super(props)
         this.state= {
             data : [],
-            loading: false
+            loading: false,
+            userSocialData: {}
         }
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleRegisterSubmit= this.handleRegisterSubmit.bind(this)
     }
     handleChange = (event) => {
         this.setState({
@@ -22,12 +29,38 @@ class App extends React.Component{
     handleSubmit = (noteSubmitted) => {
         this.loadData()
     }
+    handleSocialLogin = (userData) => {
+      this.setState({
+        userSocialData : userData
+      })
+      console.log(userData)
+    }
+    async handleRegisterSubmit(registerData){
+      let user = {
+        name: this.state.userSocialData.name,
+        email: this.state.userSocialData.email,
+        password: this.state.userSocialData.pass,
+        course: registerData.course,
+        college: registerData.college,
+        semester: registerData.semester
+      }
+      JSON.stringify(user)
+      await axios.post(`${uri}/api/socialLogin` , user)
+                  .then(response => {
+                    console.log(response)
+                    const pass = response.data.pass
+                    localStorage.setItem("pass" , pass)
+                    setTimeout(() => {
+                      localStorage.clear()
+                    }, 300000)
+                  })
+    }
     async componentDidMount(){
-        this.loadData()    
+        this.loadData()
     }
     async loadData(){
         this.setState({loading: true})
-        await axios.get('https://notes-lelo.herokuapp.com/api/notes')
+        await axios.get(`${uri}/api/notes`)
              .then(response => {
                 this.setState({
                     data : response.data.notes,
@@ -38,7 +71,7 @@ class App extends React.Component{
     }
     async handleDelete(noteDeleted){
         console.log(noteDeleted)
-        await axios.delete(`https://notes-lelo.herokuapp.com/api/notes/${noteDeleted}`)
+        await axios.delete(`${uri}/api/notes/${noteDeleted}`)
                     .then(response => {
                         console.log(response)
                         this.loadData()
@@ -51,10 +84,15 @@ class App extends React.Component{
         else {
         return(
             <div>
-                <h1>Notes Lelo Admin Panel</h1>
                 <Switch>
-                    <Route exact path="/" component = {() => <NotesList handleDelete={(deletedNote) => this.handleDelete(deletedNote)} data={this.state.data}/>} />
+                    <Route exact path="/admin" component = {(history) => (<>
+                        <h1>Notes Lelo Admin Panel</h1>
+                        <NotesList handleDelete={(deletedNote) => this.handleDelete(deletedNote)} data={this.state.data}/>
+                    </>)} />
+                    <Route path="/signup" component={(history) => <SignUpPage history={history} handleSocialLogin={(socialData) => this.handleSocialLogin(socialData)}/>} />
+                    <Route exact path="/" component = {(history) => <NotesPage history={history} data={this.state.data}/>} />
                     <Route path="/addNote" component = {(history) => <AddNotes history={history} handleSubmit={(addednote) => this.handleSubmit(addednote)} />} />
+                    <Route path="/register" component = {(history) => <Register history={history} handleRegisterSubmit={(registerData) => this.handleRegisterSubmit(registerData)}/>} />
                 </Switch>
             </div>
         )}
