53f20b29a94a25222fdb2ebb25fc9cc89be76dac
Kamal Nanda <kamalnanda45@gmail.com>
2020-08-21 20:50:42 +0530

    Add files via upload

---
+++ src/Components/Courses.js
@@ -0,0 +1,33 @@
+const courses = [
+  {
+    img: "https://ik.imagekit.io/hbj42mvqwv/BCA_QFj3iofAH5.jpg",
+    title: "Bachelor of Computer Applications",
+    link: "BCA",
+    semester: [{title: 1},{title: 2},{title: 3},{title: 4},{title: 5},{title: 6}]
+  },
+  {
+    img: "https://www.successcds.net/images/bba-courses.jpg",
+    title: "Bachelor of Buisness Administration",
+    link: "BBA",
+    semester: [{title: 1},{title: 2},{title: 3},{title: 4},{title: 5},{title: 6}]
+  },
+  {
+    img: "https://ik.imagekit.io/hbj42mvqwv/8lf083u9_NMKEdCsUwX.png",
+    title: "Bachelor of Journalism and Mass Communtication",
+    link: "BJMC",
+    semester: [{title: 1},{title: 2},{title: 3},{title: 4},{title: 5},{title: 6}]
+  },
+  {
+    img: "https://ik.imagekit.io/hbj42mvqwv/bcom_3Q8xqAa-d.jpg",
+    title: "Bachelor of Commerce",
+    link: "BCOM",
+    semester: [{title: 1},{title: 2},{title: 3},{title: 4},{title: 5},{title: 6}]
+  },
+  {
+    img: "https://ik.imagekit.io/hbj42mvqwv/eco_fD7l1Pfq_C.png",
+    title: "Economics Honors",
+    link: "ECO",
+    semester: [{title: 1},{title: 2},{title: 3},{title: 4},{title: 5},{title: 6}]
+  }
+]
+export default courses
---
+++ src/Components/Loading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+function Loading(){
+  return(
+    <div className="rotate">
+      <img src={require("./noteslelo.svg")} />
+    </div>
+  )
+}
+export default Loading
