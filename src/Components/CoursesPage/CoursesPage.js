20aade654ce41cf3c7c48335242cf539735e77f1
Kamal Nanda <kamalnanda45@gmail.com>
2020-08-21 21:02:27 +0530

    Update CoursesPage.js

--- src/Components/CoursesPage/CoursesPage.js
+++ src/Components/CoursesPage/CoursesPage.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import courses from '../Courses.js'
-import Axios from 'axios'
 import {Link} from 'react-router-dom'
 import Card from '../Card/Card'
 function CoursesPage(){
