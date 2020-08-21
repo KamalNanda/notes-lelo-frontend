aa3087f5b9ff58074b1f8510a46e1d5095969bca
Kamal Nanda <kamalnanda45@gmail.com>
2020-08-21 22:33:53 +0530

    Update CoursePage.js

--- src/Components/CoursePage/CoursePage.js
+++ src/Components/CoursePage/CoursePage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Card from '../Card/Card'
+import Card from '../Card/SubCard'
 import {Link} from 'react-router-dom'
 export default class CoursePage extends React.Component{
   render(){
