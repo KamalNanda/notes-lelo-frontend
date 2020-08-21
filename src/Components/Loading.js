3f709b127e0e6f6ae0f005159b41e9b5d9626650
Kamal Nanda <kamalnanda45@gmail.com>
2020-08-21 21:03:36 +0530

    Update Loading.js

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -2,7 +2,7 @@ import React from 'react'
 function Loading(){
   return(
     <div className="rotate">
-      <img src={require("./noteslelo.svg")} />
+      <img src={require("./noteslelo.svg")} alt="loader"/>
     </div>
   )
 }
