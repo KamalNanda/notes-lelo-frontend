68eac434f01868fe3ed85b68299fdb2a9b76aad3
Kamal Nanda <kamalnanda45@gmail.com>
2020-08-21 20:51:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,18 @@ import axios from 'axios'
 import NotesList from './Components/noteslist.js'
 import AddNotes from './Components/addNote'
 import {Switch , Route } from 'react-router'
+import Navbar from './Components/Navbar/Navbar'
+import SocialLogin from './Components/SocialLogin'
 import Register from './Components/Register'
 import NotesPage from './Components/NotesPage'
 import SignUpPage from './Components/SignUpPage'
+import LandingPage from './Components/LandingPage/LandingPage'
+import Footer from './Components/Footer/Footer'
+import CoursesPage from './Components/CoursesPage/CoursesPage'
+import CoursePage from './Components/CoursePage/CoursePage'
+import SemPage from './Components/SemPage/SemPage'
+import SubPage from './Components/SubPage/SubPage'
+import Loading from './Components/Loading'
 const uri = "https://notes-lelo.herokuapp.com"
 class App extends React.Component{
     constructor(props){
@@ -14,11 +23,13 @@ class App extends React.Component{
         this.state= {
             data : [],
             loading: false,
-            userSocialData: {}
+            userSocialData: {},
+            isLogined: false
         }
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleRegisterSubmit= this.handleRegisterSubmit.bind(this)
+        this.handleSocialRegister = this.handleSocialRegister.bind(this)
     }
     handleChange = (event) => {
         this.setState({
@@ -34,11 +45,14 @@ class App extends React.Component{
       })
       console.log(userData)
     }
+
     handleSocialLogin = (data) => {
       console.log(data)
       const pass = data.pass
       localStorage.setItem("pass" , pass)
+
     }
+
     async handleRegisterSubmit(registerData){
       let user = {
         name: this.state.userSocialData.name,
@@ -54,9 +68,9 @@ class App extends React.Component{
                     console.log(response)
                     const pass = response.data.pass
                     localStorage.setItem("pass" , pass)
+                    this.setState({isLogined : true})
+                    window.open('http://localhost:3000/notes' , "_self")
                   })
-        
-    window.open('https://notes-lelo-frontend.netlify.app/' , "_self")
     }
     async componentDidMount(){
         this.loadData()
@@ -82,21 +96,30 @@ class App extends React.Component{
     }
     render(){
         if(this.state.loading){
-            return <h2>LOADING</h2>
+            return <Loading/>
         }
         else {
         return(
-            <div>
-                <Switch>
-                    <Route exact path="/admin" component = {(history) => (<>
-                        <h1>Notes Lelo Admin Panel</h1>
-                        <NotesList handleDelete={(deletedNote) => this.handleDelete(deletedNote)} data={this.state.data}/>
-                    </>)} />
-                    <Route path="/signup" component={(history) => <SignUpPage history={history} handleSocialRegister={(Data) => this.handleSocialRegister(Data)} handleSocialLogin={(socialData) => this.handleSocialLogin(socialData)}/>} />
-                    <Route exact path="/" component = {(history) => <NotesPage history={history} data={this.state.data}/>} />
-                    <Route path="/addNote" component = {(history) => <AddNotes history={history} handleSubmit={(addednote) => this.handleSubmit(addednote)} />} />
-                    <Route path="/register" component = {(history) => <Register history={history} handleRegisterSubmit={(registerData) => this.handleRegisterSubmit(registerData)}/>} />
-                </Switch>
+            <div style={{ marginBottom: "80px"}}>
+                <Navbar/>
+                <div style={{marginTop: "10%"}}>
+                  <Switch>
+                      <Route exact path="/admin" component = {(history) => (<>
+                          <h1>Notes Lelo Admin Panel</h1>
+                          <NotesList handleDelete={(deletedNote) => this.handleDelete(deletedNote)} data={this.state.data}/>
+                      </>)} />
+                      <Route exact path="/" component={() =>   <LandingPage/>} />
+                      <Route exact path="/courses" component={() => <CoursesPage/>} />
+                      <Route exact path="/courses/:course" component={(props)=> <CoursePage {...props}/>} />
+                      <Route exact path="/courses/:course/:sem" component={(props)=> <SemPage {...props}/>} />
+                      <Route exact path="/courses/:course/:sem/:sub" component={(props) => <SubPage {...props} />} />
+                      <Route exact path="/signup" component={(history) => <SignUpPage history={history}  handleSocialRegister={(data) => this.handleSocialRegister(data)}  handleSocialLogin={(data) => this.handleSocialLogin(data)}/>} />
+                      <Route exact  path="/notes" component = {(history) => <NotesPage history={history} data={this.state.data}/>} />
+                      <Route exact path="/addNote" component = {(history) => <AddNotes history={history} handleSubmit={(addednote) => this.handleSubmit(addednote)} />} />
+                      <Route exact path="/register" component = {(history) => <Register history={history} handleRegisterSubmit={(registerData) => this.handleRegisterSubmit(registerData)}/>} />
+                  </Switch>
+                  <Footer/>
+                </div>
             </div>
         )}
     }
