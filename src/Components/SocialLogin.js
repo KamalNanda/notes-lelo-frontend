bf219979f42d4fc419bd1ee8aa78492e7229bb23
Kamal Nanda <kamalnanda45@gmail.com>
2020-08-22 06:40:48 +0530

    Update SocialLogin.js

--- src/Components/SocialLogin.js
+++ src/Components/SocialLogin.js
@@ -9,7 +9,7 @@ class SocialLogin extends Component {
       name:name,
       pass: googleId
     }
-     Axios.post(`http://localhost:2000/api/socialRegister` , userData)
+    Axios.post(`http://localhost:2000/api/socialRegister` , userData)
                 .then(response => {
                   console.log(response)
                   if(response.data.user === "false") {
@@ -21,6 +21,7 @@ class SocialLogin extends Component {
                     this.props.history.history.push('/')
                   }
                 })
+
   }
 
     render() {
@@ -29,6 +30,9 @@ class SocialLogin extends Component {
                 <GoogleLogin
                     clientId="147085937555-1momoofoqqhoalss6nibu9tne68p072e.apps.googleusercontent.com"
                     buttonText="Login with Google"
+                    render={renderProps => (
+                      <button className={this.props.className} onClick={renderProps.onClick} disabled={renderProps.disabled}><img className="g-logo" src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png" />{this.props.value}</button>
+                    )}
                     onSuccess={this.responseGoogle}
                     onFailure={this.responseGoogle}
                     cookiePolicy={'single_host_origin'}
