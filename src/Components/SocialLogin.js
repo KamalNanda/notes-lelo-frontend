5fb1414ba2703dea4d120847ef310209ea3d844d
Kamal Nanda <kamalnanda45@gmail.com>
2020-08-30 23:46:23 +0530

    Update SocialLogin.js

--- src/Components/SocialLogin.js
+++ src/Components/SocialLogin.js
@@ -21,7 +21,7 @@ class SocialLogin extends Component {
                   }
                   else {
                     this.props.handleSocialLogin(response.data)
-                    location.reload()
+                    window.location.reload()
                   }
                 })
 
