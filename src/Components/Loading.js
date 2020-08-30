83512014150ab36fc3c89ef8c2e415c5c950b5dc
Kamal Nanda <kamalnanda45@gmail.com>
2020-08-30 05:42:17 +0530

    Update Loading.js

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -2,7 +2,7 @@ import React from 'react'
 function Loading(){
   return(
     <div className="rotate">
-      <img src={require("./black.gif")} alt="loader" />
+      <img src={require("./favicon.svg")} alt="loader" />
     </div>
   )
 }
