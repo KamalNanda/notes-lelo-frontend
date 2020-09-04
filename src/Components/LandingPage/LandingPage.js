d9d87c2b9566520fda93211ba97abf1eac93af5a
Kamal Nanda <kamalnanda45@gmail.com>
2020-09-04 12:39:32 +0530

    Update LandingPage.js

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,7 +1,11 @@
 import React, {Component} from 'react'
 import './LandingPage.css'
 import {Link} from 'react-router-dom'
+import ReactGA from 'react-ga'
 export default class LandingPage extends Component{
+  componentDidMount(){
+    ReactGA.pageview(window.location.pathname);
+  }
   render(){
     return(
       <div className="landing-page">
