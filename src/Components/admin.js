c829cdd9945baf5009753a81dd68bec93521c689
Kamal Nanda <kamalnanda45@gmail.com>
2020-09-06 16:47:16 +0530

    Update admin.js

--- src/Components/admin.js
+++ src/Components/admin.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import NotesList from './noteslist.js'
 function admin(props){
-  return(
+  return(<>
     <h1>Notes Lelo Admin Panel</h1>
     <NotesList handleDelete={(deletedNote) => props.handleDelete(deletedNote)} data={props.data}/>
-  )
+  </>)
 }
 export default admin
