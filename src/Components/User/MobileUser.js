55e9bd8d85f4eec12cfe94ad26679ab0698eccd7
Kamal Nanda <kamalnanda45@gmail.com>
2020-09-06 17:39:17 +0530

    Add files via upload

---
+++ src/Components/User/MobileUser.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './User.css'
+export default class User extends React.Component{
+  render(){
+    const user = {
+      name : localStorage.getItem("user"),
+      course : localStorage.getItem("course"),
+      sem : localStorage.getItem("sem"),
+      imgUrl : localStorage.getItem("imgUrl"),
+    }
+    return(
+      <div className="user" style={{color:"white", display:"flex", alignItems: "Center"}}>
+        <img style={{borderRadius: "0px"}} src={user.imgUrl} id="userImg" alt="user-img" />
+        <div style={{marginLeft: "20px"}}>
+          <h3>{user.name}</h3>
+          <h3>{user.course} , {user.sem}</h3>
+        </div>
+      </div>
+    )
+  }
+}
