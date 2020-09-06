65f75c422b76b95515c1e8d62ccf6240b5a3aa45
Kamal Nanda <kamalnanda45@gmail.com>
2020-09-06 17:40:54 +0530

    Update PhoneNav.js

--- src/Components/Navbar/PhoneNav.js
+++ src/Components/Navbar/PhoneNav.js
@@ -1,15 +1,22 @@
 import React, {Component} from 'react'
 import SocialLogin from '../SocialLogin'
 import {Link} from 'react-router-dom'
+import User from '../User/MobileUser'
 export default class Navbar extends Component{
   constructor(props){
     super(props)
     this.state={
-      active: "home"
+      active: "home",
+      name: ''
     }
   }
   componentDidMount(){
     document.getElementById("home").classList.add("navbar-active")
+    const pass = localStorage.getItem("pass")
+    if(pass){
+        const user = localStorage.getItem("user")
+        this.setState({name: user})
+    }
   }
   onNavClick = (id) => {
     this.setState({active: id})
@@ -41,6 +48,7 @@ export default class Navbar extends Component{
 		},2000)
 	}
   render(){
+    const rightEle = (this.state.name === '') ? <SocialLogin  handleSocialRegister={(data) => this.props.handleSocialRegister(data)}  handleSocialLogin={(data) => this.props.handleSocialLogin(data)} className="login-btn" value="Login"/> : <h4 onClick={() => {localStorage.clear() ; sessionStorage.clear();  window.open("http://localhost:3000/", "_self")}} style={{color: "var(--red)",cursor: "pointer"}}>LOGOUT</h4>
     return(<>
       <div className="navbar-phone phone">
 				<div id="hamburger" onClick={this.onHamburgerClick}>
@@ -53,14 +61,15 @@ export default class Navbar extends Component{
       <div className="phone-header" ><Link to ="/"><img src={require('../noteslelo.svg')} className="logo" alt="logo" /></Link></div>
       <div className=" phone-nav"  id="phoneNav">
         <div className="navbar-left">
-
+          <span><User/></span>
           <Link to ="/"><span id="home" onClick={() => this.onNavClick("home")}>Home</span></Link>
           <Link to ="/about"><span id="about" onClick={() => this.onNavClick("about")}>About Us</span></Link>
           <Link to ="/courses"><span id="courses" onClick={() => this.onNavClick("courses")}>Courses</span></Link>
           <Link to ="/contact"><span id="contact" onClick={() => this.onNavClick("contact")}>Contact Us</span></Link>
+          <span>{rightEle}</span>
         </div>
         <div className="navbar-right">
-          <SocialLogin className="login-btn" value="Login"/>
+
         </div>
       </div>
     </>)
