eed22e23ad98806eade0faf76e1acc2ef540eceb
Kamal Nanda <kamalnanda45@gmail.com>
2020-10-31 00:14:29 +0530

    Update CoursePage.js

--- src/Components/CoursePage/CoursePage.js
+++ src/Components/CoursePage/CoursePage.js
@@ -5,29 +5,23 @@ import {Link} from 'react-router-dom'
 import ReactGA from 'react-ga'
 export default class CoursePage extends React.Component{
   onCardClick = ( sem) => {
-    sessionStorage.setItem("sem",sem)
   }
   componentDidMount(){
-    ReactGA.pageview(window.location.pathname);    
+    ReactGA.pageview(window.location.pathname);
   }
   render(){
-    const link = sessionStorage.getItem("link")
-    let courses = data.filter(function (e) {
-      return e.link === link;
-    })
-    let course = courses[0]
+    let course = this.props.history.location.state.course
     console.log(course)
+    console.log('props', this.props)
     return(<>
       <h1 className="pageHeader">{course.title}</h1>
       <div className="grid">
         {
           course.semester.map((sem , i) => {
             return (
-              <Link to={{pathname: `/courses/${link}/${sem.title}` ,link: `${link}`, sem: `${sem.title}`}}>
-                <div onClick={() => this.onCardClick(sem.title)}>
-                  <Card data={sem.title} key={i}/>
-                </div>
-              </Link>)
+                <div onClick={() => this.props.history.push({pathname: `/courses/${course.link}/${sem.title}`, state:{ sem: sem, link : course.link }})} key={i}>
+                  <Card data={sem.title}/>
+                </div>)
           })
         }
       </div>
