c26b37d77715ab1557c41f6e3f22a94ffab8436d
Kamal Nanda <kamalnanda45@gmail.com>
2020-10-31 00:13:50 +0530

    Update TypePage.js

--- src/Components/TypePage/TypePage.js
+++ src/Components/TypePage/TypePage.js
@@ -8,31 +8,80 @@ export default class SubPage extends React.Component{
     super(props)
     this.state={
       data: [],
-      contents: []
+      contents: [],
+      syl: {}
     }
   }
   onCardClick = (type) => {
     sessionStorage.setItem("type",type)
   }
   async componentDidMount(){
-    ReactGA.pageview(window.location.pathname); 
-    await axios.get(`https://notes-lelo.herokuapp.com/api/notes/${sessionStorage.getItem("link")}/${sessionStorage.getItem("sem")}/${sessionStorage.getItem("sub")}`).then(response=> this.setState({data: response.data.note}))
+    console.log(this.props)
+    ReactGA.pageview(window.location.pathname);
+          let syllabus = this.props.history.location.state.sem.subjects.find( x => x.title === this.props.history.location.state.sub)
+    await axios.get(`https://notes-lelo.herokuapp.com/api/notes/${this.props.history.location.state.link}/${this.props.history.location.state.sem.title}/${this.props.history.location.state.sub}`).then(response=> this.setState({data: response.data.note, syl: syllabus}))
     this.setState({contents : [...new Set(this.state.data.map(x => x.ctype))]})
     console.log(this.state)
   }
   render(){
     return(
       <div>
-        <h1 className="pageHeader">{sessionStorage.getItem("sub")}</h1>
+        <h1 className="pageHeader">{this.props.history.location.state.sub}</h1>
         <div className="grid">
           {
             this.state.contents.map((note, i)=>{
-              return(<div onClick={() => this.onCardClick(note)}>
-                <Link to={{pathname: `${this.props.location.pathname}/${note}` ,link: `${this.props.location.link}`, sem: `${this.props.location.sem}`, sub: `${this.props.location.sub}`, type: note}}><Card data={note} key={i} /></Link>
+              return(<div onClick={() => this.props.history.push({
+                pathname: `/courses/${this.props.history.location.state.link}/${this.props.history.location.state.sem.title}/${this.props.history.location.state.sub}/${note}`,
+                state: {
+                  link : this.props.history.location.state.link,
+                  sub : this.props.history.location.state.sub,
+                  sem: this.props.history.location.state.sem.title,
+                  type: note
+
+                }
+              })}>
+                <Card data={note} key={i} />
               </div>)
             })
           }
+          {/*<Link to={{, sem: `${this.props.location.sem}`, sub: `${this.props.location.sub}`, type: note}}>*/}
         </div>
+        {
+          this.state.syl ? (<>
+            <h1 className="pageHeader">Syllabus</h1>
+            <div className="container">
+              <h2> Paper Id - {this.state.syl.paper_id}</h2>
+              {
+                this.state.syl.syllabus ? (<> 
+                    <h3>UNIT 1 </h3>
+                    <h4>{this.state.syl.syllabus.unit_1.title}</h4>
+                    <p>{this.state.syl.syllabus.unit_1.chapters}</p>
+                  </>): <></>
+              }
+              {
+                this.state.syl.syllabus ? (<> 
+                    <h3>UNIT 2 </h3>
+                    <h4>{this.state.syl.syllabus.unit_2.title}</h4>
+                    <p>{this.state.syl.syllabus.unit_2.chapters}</p>
+                  </>): <></>
+              }
+              {
+                this.state.syl.syllabus ? (<> 
+                    <h3>UNIT 3 </h3>
+                    <h4>{this.state.syl.syllabus.unit_3.title}</h4>
+                    <p>{this.state.syl.syllabus.unit_3.chapters}</p>
+                  </>): <></>
+              }
+              {
+                this.state.syl.syllabus ? (<> 
+                    <h3>UNIT 4 </h3>
+                    <h4>{this.state.syl.syllabus.unit_4.title}</h4>
+                    <p>{this.state.syl.syllabus.unit_4.chapters}</p>
+                  </>): <></>
+              }
+            </div>
+          </>) : <></>
+        }
       </div>
     )
   }
