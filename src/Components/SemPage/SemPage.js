ceee899ed0469cb8897457096adf8fa7b6da830c
Kamal Nanda <kamalnanda45@gmail.com>
2020-10-31 00:12:56 +0530

    Update SemPage.js

--- src/Components/SemPage/SemPage.js
+++ src/Components/SemPage/SemPage.js
@@ -14,21 +14,29 @@ export default class SemPage extends React.Component{
   onCardClick = (sub) => {
     sessionStorage.setItem("sub",sub)
   }
-  async componentDidMount(){
+  async componentDidMount(){    console.log(this.props)
     ReactGA.pageview(window.location.pathname);
-    await axios.get(`https://notes-lelo.herokuapp.com/api/notes/${sessionStorage.getItem("link")}/${sessionStorage.getItem("sem")}`).then(response=> this.setState({data: response.data.note}))
+    await axios.get(`https://notes-lelo.herokuapp.com/api/notes/${this.props.history.location.state.link}/${this.props.history.location.state.sem.title}`).then(response=> {
+      this.setState({data: response.data.note})
+      console.log(response)
+    })
     this.setState({subjects : [...new Set(this.state.data.map(x => x.subject))]})
-    console.log(this.props)
+
   }
   render(){
     return(
       <div>
-        <h1 className="pageHeader">Semester - {sessionStorage.getItem("sem")}</h1>
+        <h1 className="pageHeader">Semester - {this.props.history.location.state.sem.title}</h1>
         <div className="grid">
           {
             this.state.subjects.map((subject , i) => {
-              return (<div onClick={() => this.onCardClick(subject)}>
-                <Link to={{pathname: `/courses/${this.props.location.link}/${this.props.location.sem}/${subject}`, link: `${this.props.location.link}`, sem: `${this.props.location.sem}`, sub: `${subject}` }} ><Card data={subject} key={i} /></Link>
+              
+              return (<div 
+                onClick={() => this.props.history.push({
+                  pathname: `/courses/${this.props.history.location.state.link}/${this.props.history.location.state.sem.title}/${subject}`,
+                  state: {sem: this.props.history.location.state.sem, link : this.props.history.location.state.link, sub: subject}
+                })}>
+                <Card data={subject} key={i} />
               </div>)
             })
           }
@@ -37,3 +45,4 @@ export default class SemPage extends React.Component{
     )
   }
 }
+//<Link to={{, syl: this.props.location.subs, link: `${this.props.location.link}`, sem: `${this.props.location.sem}`, sub: `${subject}` }} >
