622a0d3d2adeaed5083f3de7959ca37a47519a36
Kamal Nanda <kamalnanda45@gmail.com>
2020-10-31 00:17:31 +0530

    Update CoursesPage.js

--- src/Components/CoursesPage/CoursesPage.js
+++ src/Components/CoursesPage/CoursesPage.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import courses from '../Courses.js'
-import {Link} from 'react-router-dom'
-import {useHistory} from 'react-router-dom'
 import Card from '../Card/Card'
 import ReactGA from 'react-ga'
 function CoursesPage(props){
@@ -9,7 +7,6 @@ function CoursesPage(props){
   React.useEffect(() => {
     ReactGA.pageview(window.location.pathname);
   })
-  const history = useHistory()
   console.log(props)
   return(
     <div>
