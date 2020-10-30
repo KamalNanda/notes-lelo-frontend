2f65ba46e9d3c40e961675206d205d61d3407e1f
Kamal Nanda <kamalnanda45@gmail.com>
2020-10-31 00:22:28 +0530

    Add files via upload

---
+++ src/Components/AboutPage/AboutPage.js
@@ -0,0 +1,27 @@
+import React from 'react'
+const AboutPage = () => {
+  return(
+    <div>
+      <h1 className="pageHeader">About Page</h1>
+      <div className="container">
+      <h2 className="pageHeader">How it all started: </h2>
+        <p>
+        Being the college student ourselves with a constant need for class notes and study materials, we took the easiest way out i.e. via WhatsApp groups.
+        With a gradual increase in syllabus and subjects, Whatsapp failed to gather all the notes at one place. Google Drive was an option, but an ineffective one.
+        The lightning struck on me one day and we created a website www.jayesmakkar.me for our fellow classmates where they could easily and effortlessly access the study material.
+        <br/><br/>
+        In a span of three months, our website catered to over 3000 students. This generous response invigorated us to start Noteslelo. The journey commenced with BCA and reached to various courses of GGSIPU.
+        <br/><br/>
+        Noteslelo not only provides quality notes and snippets from books which ought to be read before an exam, but also provides simpler and legible ways to solve and learn concepts. However, the most important and extravagant feature still remains the ANALYSIS. It is a compiled document of the most important questions which are most likely to occur in the question paper, based on the previous year patterns. The questions are scrutinized well for our buddies who love to study at the last minute under extreme pressure, just like us.
+        </p>
+      <h2 className="pageHeader">The new beginning: </h2>
+        <p>
+        With 11,000+ users and 100,000+ page views in less than a year, noteslelo is succeeding with flying colours. The huge success of previous versions of noteslelo has encouraged us to venture into a broader service for the students. We introduce the SOCIAL MEDIA FOR NOTES for students, with a new set of features that would enable them to share notes effectively. It will be a one-stop destination where you can easily search for and share all the necessary class notes plus mandatory exam materials.
+        <br/><br/>
+        This website is oriented towards the goal of providing quality study materials that are easily accessible to each and every GGSIPU student without any limitations.
+        </p>
+      </div>
+    </div>
+  )
+}
+export default AboutPage
