d72e79224546ce4cb43050a54c4812c1548c905a
Kamal Nanda <kamalnanda45@gmail.com>
2020-10-31 00:19:14 +0530

    Update LandingPage.js

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
 import './LandingPage.css'
-import {Link} from 'react-router-dom'
 import ReactGA from 'react-ga'
 export default class LandingPage extends Component{
   componentDidMount(){
