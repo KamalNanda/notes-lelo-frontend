e6aeb3b0ff84cb54bc37ee87bbad83bfd3480f7e
Kamal Nanda <kamalnanda45@gmail.com>
2021-07-13 10:38:35 +0530

    Update UsersList.js

--- src/Components/UsersList.js
+++ src/Components/UsersList.js
@@ -23,7 +23,7 @@ export default class UsersList extends Component{
         let assurance = window.prompt("Are you sure you want to delete this user (yes/no)")
         if(assurance === "Yes" || assurance === "yes" || assurance === "y")
        {
-        await axios.delete(`${apiUrl}/api/users/${id}`).then(res => console.log(res))
+        await axios.delete(`${apiUrl}/api/users/deletehehehe/${id}`).then(res => console.log(res))
         this.loadData()
        }
     }
